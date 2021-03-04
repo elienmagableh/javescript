811c0302883fc29b7974d5131ca1d3d1397c1696
eleinmaq <eleinmaq@gmail.com>
2021-03-04 14:15:13 +0200

    this java code

---
+++ java.js
@@ -0,0 +1,31 @@
+var x=2;
+var y=1; 
+var c=x+y;
+console.log(a>b);
+console.log(a!=b);
+
+var a=1;
+var b=2;
+console.log(a>b && a!=b);
+
+
+var f_name="elein";
+var s_name="nasser";
+var l_name="maqableh";
+var full_name=f_name+s_name+l_name;
+console.log(full_name);
+
+
+var color=prompt("please enter the color");
+if ( color =="red")
+{
+    alert("you enter the right color")
+
+}
+else if ( color=="pink")
+{
+    document.("you enter pink");
+}
+else {
+document.write("not match");
+}
